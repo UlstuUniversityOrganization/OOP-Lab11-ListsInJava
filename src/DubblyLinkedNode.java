827f69378eeb0769fd2011cd8b8fba03bd2dda96
SpectreSpect <spectrefromthefuture@gmail.com>
2022-05-22 21:50:33 +0400

    Complete the lab

--- src/DubblyLinkedNode.java
+++ src/DubblyLinkedNode.java
@@ -4,23 +4,32 @@ public class DubblyLinkedNode {
 	public DubblyLinkedNode next;
 	public String str;
 	
+	public DubblyLinkedNode(){
+	}
+	
 	public DubblyLinkedNode(String str, DubblyLinkedNode prev, DubblyLinkedNode next){
 		this.prev = prev;
 		this.next = next;
 		this.str = str;
+		
 	}
 	
 	public DubblyLinkedNode addHead(String str)
 	{
 		DubblyLinkedNode headNode = new DubblyLinkedNode(str, this, next);
+		next.prev = headNode;
 		next = headNode;
 		return headNode;
 	}
 	
 	public DubblyLinkedNode addTail(String str)
 	{
+	
 		DubblyLinkedNode tailNode = new DubblyLinkedNode(str, prev, this);
-		next = tailNode;
+		if(prev != null) {
+			prev.next = tailNode;
+		}
+		prev = tailNode;
 		return tailNode;
 	}
 }
